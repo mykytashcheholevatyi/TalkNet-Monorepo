f5b8c8a36051185bca41c90a90fcaefa89f2e57b
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-04 20:57:46 +0100

    1

--- reddit-clone/src/App.js
+++ reddit-clone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 3333 and save to reload.
+          Edit <code>src/App.js</code> 3333444 and save to reload.
         </p>
         <a
           className="App-link"
