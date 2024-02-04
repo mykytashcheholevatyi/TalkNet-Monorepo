baff8a1cbdddd571671b9d79570148c03a001069
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-04 21:45:28 +0100

    1

--- reddit-clone/src/App.js
+++ reddit-clone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> четвертый тест and save to reload.
+          Edit <code>src/App.js</code> 5  тест and save to reload.
         </p>
         <a
           className="App-link"
