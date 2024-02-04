0662dbf48e0e10d660d09b5fb6c063ea80b0e2ed
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-04 22:13:14 +0100

    Update App.js

--- reddit-clone/src/App.js
+++ reddit-clone/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // Импорт страниц
-import HomePage from './pages/HomePage';
-import PostCreationPage from './pages/PostCreationPage';
 import CommunityCreationPage from './pages/CommunityCreationPage';
 import CommunityPage from './pages/CommunityPage';
+import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import PostCreationPage from './pages/PostCreationPage';
 import PostPage from './pages/PostPage';
 import ProfilePage from './pages/ProfilePage';
 import RegisterPage from './pages/RegisterPage';
@@ -19,26 +18,17 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <Link to="/" className="App-link">Home</Link>
-          {/* Добавьте ссылки на другие страницы здесь */}
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-        </header>
-
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/create-post" element={<PostCreationPage />} />
-          <Route path="/create-community" element={<CommunityCreationPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/community/create" element={<CommunityCreationPage />} />
           <Route path="/community/:id" element={<CommunityPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/post/create" element={<PostCreationPage />} />
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/profile/:id" element={<ProfilePage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/search" element={<SearchResultsPage />} />
           <Route path="/settings" element={<SettingsPage />} />
-          {/* Дополнительные маршруты могут быть добавлены здесь */}
         </Routes>
       </div>
     </Router>
