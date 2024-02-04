90e6b10056a7ae274353cb8d03ce294251625be7
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-04 23:45:32 +0100

    1

---
+++ reddit-clone/src/pages/HomePageRU.js
@@ -0,0 +1,13 @@
+// В файле src/pages/HomePage.js
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the Home Page of Forulink!</p>
+    </div>
+  );
+}
+
+export default HomePage;
