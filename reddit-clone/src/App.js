a54b459d3e80ebc698d25d9428dd11255e878f8d
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-04 22:14:38 +0100

    Update App.js

--- reddit-clone/src/App.js
+++ reddit-clone/src/App.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import logo from './logo.svg'; // Путь к лого
+import './App.css'; // Подключение стилей
 
 // Импорт страниц
-import CommunityCreationPage from './pages/CommunityCreationPage';
-import CommunityPage from './pages/CommunityPage';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import PostCreationPage from './pages/PostCreationPage';
-import PostPage from './pages/PostPage';
-import ProfilePage from './pages/ProfilePage';
-import RegisterPage from './pages/RegisterPage';
-import SearchResultsPage from './pages/SearchResultsPage';
-import SettingsPage from './pages/SettingsPage';
+// Другие импорты...
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <nav>
+            <Link to="/" className="App-link">Home</Link>
+            {/* Добавьте другие ссылки для навигации */}
+          </nav>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/community/create" element={<CommunityCreationPage />} />
-          <Route path="/community/:id" element={<CommunityPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/post/create" element={<PostCreationPage />} />
-          <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/profile/:id" element={<ProfilePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/search" element={<SearchResultsPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          {/* Определите другие маршруты здесь */}
         </Routes>
       </div>
     </Router>
