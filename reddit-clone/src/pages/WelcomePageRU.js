08a03a179ff5f18cbddef90102d02921470d2bb1
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-06 19:49:24 +0100

    1

---
+++ reddit-clone/src/pages/WelcomePageRU.js
@@ -0,0 +1,14 @@
+// WelcomePageRU.js
+import React from 'react';
+
+function WelcomePageRU() {
+  return (
+    <div>
+      <h1>Добро пожаловать на наш форум</h1>
+      <p>Это русская версия нашего форума.</p>
+      {/* Добавьте другие элементы с текстами */}
+    </div>
+  );
+}
+
+export default WelcomePageRU;
