6cf3f9b9b9d579b893196e96dcc12b55ae75ddc7
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-07 02:31:35 +0100

    Update app.py

--- backend/auth-service/app.py
+++ backend/auth-service/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify, render_template_string
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate  # Импорт Flask-Migrate
 
 app = Flask(__name__)
 
 # Конфигурация базы данных
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # Инициализация Flask-Migrate
 
 # Модель пользователя
 class User(db.Model):
@@ -44,5 +46,4 @@ def index():
     return '<h1>Welcome to the Flask App!</h1><p>Go to <a href="/users">/users</a> to see the list of users.</p>'
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(host='0.0.0.0', port=8000, debug=True)
