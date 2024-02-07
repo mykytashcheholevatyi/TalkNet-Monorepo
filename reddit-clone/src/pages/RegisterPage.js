f7311e4fd180197ed6518a79366ebab63f7df9e5
Mykyta Shcheholevatyi Oleksiiovych <tosniki91@gmail.com>
2024-02-07 18:02:58 +0100

    Update RegisterPage.js

--- reddit-clone/src/pages/RegisterPage.js
+++ reddit-clone/src/pages/RegisterPage.js
@@ -80,7 +80,7 @@ function RegisterPage() {
   return (
     <div className="register-page">
       <h1>Register</h1>
-      <p>Create a new account to join the community.</p>
+      <p>Create TEST a new account to join the community.</p>
       {errors.form && <div className="form-error">{errors.form}</div>}
       <form onSubmit={handleSubmit} noValidate>
         <div className="form-group">
