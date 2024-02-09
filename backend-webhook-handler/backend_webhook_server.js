bda6d905c7c615e0220cf0b8654fd8b82dc8ab2f
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-09 17:57:45 +0100

    Update backend_webhook_server.js

--- backend-webhook-handler/backend_webhook_server.js
+++ backend-webhook-handler/backend_webhook_server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const { exec } = require('child_process');
 const crypto = require('crypto');
 const app = express();
-const port = 9001; // Убедитесь, что этот порт свободен на вашем сервере
+const port = 9001; // Ensure this port is free on your server
 
-app.use(express.json()); // Поддержка JSON-encoded bodies
+app.use(express.json()); // Support for JSON-encoded bodies
 
-const secret = 'your_webhook_secret'; // Используйте ваш секретный код
+const secret = 'your_webhook_secret'; // Use your actual webhook secret here
 
 app.post('/backend-webhook', (req, res) => {
     const signature = req.headers['x-hub-signature-256'] || '';
@@ -14,22 +14,24 @@ app.post('/backend-webhook', (req, res) => {
     const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
 
     if (signature !== digest) {
+        console.error('Mismatched signatures');
         return res.status(401).send('Mismatched signatures');
     }
 
-    // Проверка на обновление ветки main
+    // Check for updates to the main branch
     const branch = req.body.ref;
     if(branch === 'refs/heads/main'){
-        exec('bash /srv/talknet/backend/scripts/update_and_restart.sh', (err, stdout, stderr) => { // Изменено на update_and_restart.sh
+        exec('bash /srv/talknet/backend/scripts/update_and_restart.sh', (err, stdout, stderr) => {
             if (err) {
                 console.error(`exec error: ${err}`);
                 return res.status(500).send('Server error');
             }
             console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
+            if (stderr) console.error(`stderr: ${stderr}`);
             res.status(200).send('Backend successfully updated and restarted');
         });
     } else {
+        console.log('Push was not to main branch, no action taken');
         res.status(200).send('Push was not to main branch, no action taken');
     }
 });
