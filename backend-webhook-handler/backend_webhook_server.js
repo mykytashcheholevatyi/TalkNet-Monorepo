275841c4aaa39c79a62dc081964c5d3a02b14070
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-09 18:14:23 +0100

    Update backend_webhook_server.js

--- backend-webhook-handler/backend_webhook_server.js
+++ backend-webhook-handler/backend_webhook_server.js
@@ -2,40 +2,41 @@ const express = require('express');
 const { exec } = require('child_process');
 const crypto = require('crypto');
 const app = express();
-const port = 9001; // Ensure this port is free on your server
+const port = 9001; // Убедитесь, что этот порт свободен на вашем сервере
 
-app.use(express.json()); // Support for JSON-encoded bodies
+app.use(express.json()); // Поддержка JSON-кодированных тел запросов
 
-const secret = 'your_webhook_secret'; // Use your actual webhook secret here
+const secret = 'your_webhook_secret'; // Используйте ваш фактический секрет веб-хука здесь
 
 app.post('/backend-webhook', (req, res) => {
     const signature = req.headers['x-hub-signature-256'] || '';
     const hmac = crypto.createHmac('sha256', secret);
-    const digest = 'sha256=' + hmac.update(JSON.stringify(req.body)).digest('hex');
+    hmac.update(JSON.stringify(req.body));
+    const digest = 'sha256=' + hmac.digest('hex');
 
     if (signature !== digest) {
-        console.error('Mismatched signatures');
-        return res.status(401).send('Mismatched signatures');
+        console.error('Несовпадение подписей');
+        return res.status(401).send('Несовпадение подписей');
     }
 
-    // Check for updates to the main branch
+    // Проверка обновлений в главной ветке
     const branch = req.body.ref;
-    if(branch === 'refs/heads/main'){
+    if (branch === 'refs/heads/main') {
         exec('bash /srv/talknet/backend/scripts/update_and_restart.sh', (err, stdout, stderr) => {
             if (err) {
-                console.error(`exec error: ${err}`);
-                return res.status(500).send('Server error');
+                console.error(`Ошибка exec: ${err}`);
+                return res.status(500).send('Ошибка сервера');
             }
             console.log(`stdout: ${stdout}`);
             if (stderr) console.error(`stderr: ${stderr}`);
-            res.status(200).send('Backend successfully updated and restarted');
+            res.status(200).send('Бэкенд успешно обновлен и перезапущен');
         });
     } else {
-        console.log('Push was not to main branch, no action taken');
-        res.status(200).send('Push was not to main branch, no action taken');
+        console.log('Пуш был не в главную ветку, действий не предпринимается');
+        res.status(200).send('Пуш был не в главную ветку, действий не предпринимается');
     }
 });
 
 app.listen(port, () => {
-    console.log(`Backend webhook handler running on port ${port}`);
+    console.log(`Обработчик веб-хука бэкенда запущен на порту ${port}`);
 });
