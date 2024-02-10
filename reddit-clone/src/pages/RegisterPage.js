aa05873cd2d149df12fe8d76b0228d9e64b62636
Mykyta Shcheholevatyi <mykytashch@hotmail.com>
2024-02-10 01:26:39 +0100

    2

--- reddit-clone/src/pages/RegisterPage.js
+++ reddit-clone/src/pages/RegisterPage.js
@@ -80,7 +80,7 @@ function RegisterPage() {
   return (
     <div className="register-page">
       <h1>Register</h1>
-      <p>Create TEST 777 a new account to join the community.</p>
+      <p>Create TEST 77722 a new account to join the community.</p>
       {errors.form && <div className="form-error">{errors.form}</div>}
       <form onSubmit={handleSubmit} noValidate>
         <div className="form-group">
